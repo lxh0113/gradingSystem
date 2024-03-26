19e25a7477c7517e7510d8f0a934544e60b865a3
裴雨孜 <2171204141@qq.com>
2024-03-26 11:27:19 +0800

    冲突

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -239,7 +239,7 @@ const adminNavList=ref([
     {text:"账号管理",icon:"iconfont icon-user-management",to:"/admin/accountManagement",isHaveNext:false,childrenList:[]}
 ])
 
-const leftList=teacherNavList.value
+const leftList=studentNavList.value
 
 const userInfoForm = ref(null)
 const newName=ref("")
